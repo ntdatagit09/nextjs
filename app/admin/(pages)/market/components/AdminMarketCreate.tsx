7f8afa5afee4a9ccd81f535ce8582d24ce69dd44
DESKTOP-3I593JA\Admin <ntdatagit09@gmail.com>
2024-05-17 13:34:07 +0700

    fix some bug

--- app/admin/(pages)/market/components/AdminMarketCreate.tsx
+++ app/admin/(pages)/market/components/AdminMarketCreate.tsx
@@ -1,13 +1,13 @@
-import CustomEditor from '@/components/admin/CustomEditor'
+// import CustomEditor from '@/components/admin/CustomEditor'
 import React from 'react'
 
 const AdminMarketCreate = () => {
     return (
         <div>
             <h1>Thêm bài viết tuyến tour</h1>
-            <CustomEditor
+            {/* <CustomEditor
                 initialData='<h1>Hello from CKEditor in Next.js!</h1>'
-            />
+            /> */}
         </div>
     )
 }
